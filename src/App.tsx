36c5e8d3538d99ef6301e3e79c40e74b0b2a274d
bao <baomai@berkeley.edu>
2024-07-28 18:11:18 -0700

    send 1 SOL works

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Connection, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+
+
+const LOCAL_RPC_URL = "http://127.0.0.1:8899"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [solBalance, setSolBalance] = useState<number>()
+
+  useEffect(() => {
+    const connectProvider = async () => {
+      const provider = getProvider(); // see "Detecting the Provider"
+      const connection = new Connection(LOCAL_RPC_URL)
+
+      try {
+        const resp = await provider.connect();
+        console.log(resp.publicKey.toString());
+        const publicKey = resp.publicKey;
+        const balance = await connection.getBalance(publicKey);
+        setSolBalance(balance / 1e9)
+      } catch (err) {
+        // { code: 4001, message: 'User rejected the request.' }
+      }
+    };
+
+    connectProvider();
+  }, [])
+
+  const send1SOL = useCallback(async () => {
+    const provider = getProvider(); // see "Detecting the Provider"
+    const targetPubkey = "73fCqk4vhrUH3V84Vqf4BMt6ngPhe6dccH5yN5AVwFWw";
+    const connection = new Connection(LOCAL_RPC_URL)
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: provider.publicKey,
+        lamports: 1e9,
+        toPubkey: new PublicKey(targetPubkey)
+      })
+    );
+
+    transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+    transaction.feePayer = provider.publicKey;
+    try {
+      const signed = await provider.signTransaction(transaction);
+      console.log("Signed:", signed);
+
+      const signature = await connection.sendRawTransaction(signed.serialize());
+      console.log("Signature:", signature);
+
+      const confirmation = await connection.confirmTransaction(signature);
+      console.log("Confirmation:", confirmation);
+
+    } catch (err) {
+      console.log("Error:", err);
+    }
+  }, [])
 
   return (
     <>
@@ -18,9 +71,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <p>
+          SOL Balance: {solBalance}
+        </p>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '10px'
+        }}>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={send1SOL}>
+            Send 1 SOL
+          </button>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -31,5 +96,19 @@ function App() {
     </>
   )
 }
+
+const getProvider = () => {
+  if ('phantom' in window) {
+    // @ts-expect-error no  global window 
+    const provider = window.phantom?.solana;
+
+    if (provider?.isPhantom) {
+      return provider;
+    }
+    alert('Please install Phantom wallet')
+  }
+
+  window.open('https://phantom.app/', '_blank');
+};
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { Buffer } from 'buffer'
+// @ts-expect-error polyfill
+globalThis.Buffer = Buffer
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
