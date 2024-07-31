b07414674a902864713e665dd831006e2566afcc
bao <baomai@berkeley.edu>
2024-07-31 13:46:33 -0700

    tailwindcss

---
+++ src/addr-gating/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (<div className="h-screen w-screen">
+    <div className="flex w-full justify-center items-center h-full">
+      <p>
+        Hello World
+      </p>
+    </div>
+  </div>)
+}
+
+export default App
