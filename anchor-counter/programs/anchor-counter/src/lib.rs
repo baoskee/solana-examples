e246ab227585adf4c0ed01370943c14b6d5a0fba
bao <baomai@berkeley.edu>
2024-07-31 16:35:15 -0700

    counter logic

--- anchor-counter/programs/anchor-counter/src/lib.rs
+++ anchor-counter/programs/anchor-counter/src/lib.rs
@@ -3,13 +3,43 @@ use anchor_lang::prelude::*;
 declare_id!("8rK93pjtsMfxvLayRD8Ypa9YJCursWoCT7379KTJtzxb");
 
 #[program]
-pub mod anchor_counter {
+pub mod simple_counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Update>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count -= 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+    pub system_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
