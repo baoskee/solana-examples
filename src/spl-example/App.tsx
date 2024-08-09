979026a5e403a7366e3f94104fae035af0af692f
bao <baomai@berkeley.edu>
2024-08-09 15:41:07 -0700

    see mint details

--- src/spl-example/App.tsx
+++ src/spl-example/App.tsx
@@ -1,6 +1,7 @@
 import {
   Connection,
   Keypair,
+  PublicKey,
   SystemProgram,
   Transaction,
 } from "@solana/web3.js"
@@ -9,38 +10,60 @@ import "../index.css"
 import { useCallback, useState } from "react"
 import { LOCAL_RPC_URL } from "../lib/constants";
 import { getProvider } from "../lib/util";
-import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint, getMint, MINT_SIZE, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import { useQuery } from "@tanstack/react-query";
+
+const DEFAULT_MINT = 'HRHCBGxwW7Qb6KU9GcnwR32vQ8wu5m1N8pzyqvaTcAq4';
 
 function App() {
-  const [mint, setMint] = useState<string | null>(null);
+  const [mint, setMint] = useState<string | null>(DEFAULT_MINT);
+  const { data: mintAccount, refetch: refetchMintAccount } = useQuery({
+    queryKey: ['mintAccount', mint],
+    queryFn: async () => {
+      try {
+        const connection = new Connection(LOCAL_RPC_URL);
+        const res = await getMint(connection, new PublicKey(mint!),
+          undefined,
+          TOKEN_2022_PROGRAM_ID
+        );
+      console.log('res', res);
+      return res;
+      } catch (e) {
+        console.error(e);
+        return null;
+      }
+    },
+    enabled: !!mint,
+  });
 
   const createTokenMintAccount = useCallback(async () => {
     const connection = new Connection(LOCAL_RPC_URL);
     const latestBlockhash = await connection.getLatestBlockhash();
     console.log('latestBlockhash', latestBlockhash);
 
+
     const provider = getProvider();
-    if (!provider) 
+    if (!provider)
       return alert('Please install Phantom wallet');
 
     await provider.connect();
     const mint = Keypair.generate();
     const lamports = await getMinimumBalanceForRentExemptMint(connection);
     const transaction = new Transaction().add(
-        SystemProgram.createAccount({
-            fromPubkey: provider.publicKey,
-            newAccountPubkey: mint.publicKey,
-            space: MINT_SIZE,
-            lamports,
-            programId: TOKEN_2022_PROGRAM_ID,
-        }),
-        createInitializeMint2Instruction(
-          mint.publicKey, 
-          8, 
-          provider.publicKey, // mint authority
-          provider.publicKey, // freeze authority
-          TOKEN_2022_PROGRAM_ID
-        )
+      SystemProgram.createAccount({
+        fromPubkey: provider.publicKey,
+        newAccountPubkey: mint.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId: TOKEN_2022_PROGRAM_ID,
+      }),
+      createInitializeMint2Instruction(
+        mint.publicKey,
+        8,
+        provider.publicKey, // mint authority
+        provider.publicKey, // freeze authority
+        TOKEN_2022_PROGRAM_ID
+      )
     );
 
     transaction.recentBlockhash = latestBlockhash.blockhash;
@@ -51,16 +74,26 @@ function App() {
     const confirmation = await connection.confirmTransaction(sig);
     console.log("Confirmation:", confirmation);
     setMint(mint.publicKey.toString());
-  }, []);
+    refetchMintAccount();
+  }, [refetchMintAccount]);
 
-  return <div className="h-screen w-screen flex items-center justify-center">
-    <div>
+  return <div className="h-screen w-screen items-center justify-center flex flex-col">
+    {mint && <div>
       <p>Mint address: {mint}</p>
-    </div>
+      <div>Mint account: <pre className="text-xs">{stringifyBigInt(mintAccount)}</pre></div>
+    </div>}
     <button onClick={createTokenMintAccount}>
       Create token mint account
     </button>
   </div>
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const stringifyBigInt = (obj: any) => {
+  return JSON.stringify(obj, (_, value) =>
+    typeof value === 'bigint' ? value.toString() : value, 2);
+};
+
+
 
 export default App
