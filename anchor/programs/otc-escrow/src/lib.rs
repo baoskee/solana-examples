9e65fdb92c998f7a8eead82b61c6024d8ce5149b
bao <baomai@berkeley.edu>
2024-08-11 01:15:49 -0700

    completed claim logic

--- anchor/programs/otc-escrow/src/lib.rs
+++ anchor/programs/otc-escrow/src/lib.rs
@@ -12,8 +12,8 @@ pub mod escrow {
     pub fn initialize(
         ctx: Context<Initialize>, 
         id_seed: u64,
-        token_maker_amount: u64,
-        token_taker_amount_wanted: u64, 
+        token_a_amount: u64,
+        token_b_amount: u64,
         taker: Pubkey,
     ) -> Result<()> {
         // 1. transfer the tokens from the maker to the contract_token_account  
@@ -31,7 +31,7 @@ pub mod escrow {
 
         transfer_checked(
             cpi_context,
-            token_maker_amount,
+            token_a_amount,
             ctx.accounts.token_mint_a.decimals,
         )?;
 
@@ -40,9 +40,9 @@ pub mod escrow {
             maker: ctx.accounts.maker.key(),
             taker,
             token_mint_a: ctx.accounts.token_mint_a.key(),
-            token_maker_amount,
+            token_a_amount,
             token_mint_b: ctx.accounts.token_mint_b.key(),
-            token_taker_amount: token_taker_amount_wanted,
+            token_b_amount,
             // notice client will provide id but the bump is calculated by Anchor
             id_seed,
             bump: ctx.bumps.otc_offer,
@@ -52,7 +52,53 @@ pub mod escrow {
     }
 
     pub fn claim(ctx: Context<Claim>) -> Result<()> {
-        return Err(ErrorCode::RequireViolated.into());
+        // 1. transfer token b to maker 
+        let transfer_accounts = TransferChecked {
+            from: ctx.accounts.token_account_taker_b.to_account_info(),
+            mint: ctx.accounts.token_mint_b.to_account_info(),
+            to: ctx.accounts.token_account_maker_b.to_account_info(),
+            authority: ctx.accounts.taker.to_account_info(),
+        };
+
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_accounts,
+        );
+
+        transfer_checked(
+            cpi_context, 
+            ctx.accounts.otc_offer.token_b_amount, 
+            ctx.accounts.token_mint_b.decimals,
+        )?;
+
+        // 2. transfer vault tokens to taker 
+        let transfer_accounts = TransferChecked {
+            from: ctx.accounts.contract_token_account.to_account_info(),
+            mint: ctx.accounts.token_mint_a.to_account_info(),
+            to: ctx.accounts.token_account_taker_a.to_account_info(),
+            authority: ctx.accounts.otc_offer.to_account_info(),
+        };
+
+        let signer_seeds: [&[&[u8]]; 1] = [&[
+            b"otc_offer",
+            ctx.accounts.maker.to_account_info().key.as_ref(),
+            &ctx.accounts.otc_offer.id_seed.to_le_bytes()[..],
+            &[ctx.accounts.otc_offer.bump],
+        ]];
+
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_accounts,
+        ).with_signer(&signer_seeds);
+
+        transfer_checked(
+            cpi_context,
+            ctx.accounts.otc_offer.token_a_amount,
+            ctx.accounts.token_mint_a.decimals,
+        )?;
+
+        // @todo close all accounts to save on storage costs
+        Ok(())
     }
 }
 
@@ -108,9 +154,9 @@ pub struct OtcOffer {
     // address of the taker. Only the taker can claim the tokens.
     pub taker: Pubkey,
     pub token_mint_a: Pubkey,
-    pub token_maker_amount: u64,
+    pub token_a_amount: u64,
     pub token_mint_b: Pubkey,
-    pub token_taker_amount: u64,
+    pub token_b_amount: u64,
 
     // `id_seed` is an input to derive the PDA seed
     // makes for 2**64 possible offers given the same maker address
@@ -148,6 +194,16 @@ pub struct Claim<'info> {
         bump = otc_offer.bump,
     )]
     pub otc_offer: Account<'info, OtcOffer>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = otc_offer,
+        associated_token::token_program = token_program
+    )]
+    pub contract_token_account: Account<'info, TokenAccount>,
+
+    // @todo add init_if_needed constraint to (some) accounts below
 
     // needs taker account for token b (for transfer to a)
     #[account(
