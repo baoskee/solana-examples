b495d348f5a26e0c433c929e02f4b702b54f6219
bao <baomai@berkeley.edu>
2024-08-11 18:41:53 -0700

    offer data fetched

--- next-client/src/app/escrow/page.tsx
+++ next-client/src/app/escrow/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import { connectAnchorWallet, LOCAL_RPC_URL } from "@/lib/util"
-import { Connection } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 import * as anchor from '@coral-xyz/anchor';
 import { useCallback } from "react";
 import { escrowIDL, Escrow } from "anchor-local";
-import { sign } from "crypto";
+import { useQuery } from "@tanstack/react-query";
 
 // Fill out your params here
 const TOKEN_MINT_A = "5CiESJk1uYGZ82S4YhaC5YCjRbUX1J4Q6Xf2ZWmYC6g7"
@@ -13,6 +13,8 @@ const TOKEN_MINT_A_AMOUNT = 100
 const TOKEN_MINT_B = "5CiESJk1uYGZ82S4YhaC5YCjRbUX1J4Q6Xf2ZWmYC6g7"
 const TOKEN_MINT_B_AMOUNT = 200
 const TAKER = "HNc5mQKb5X7Agsk866kxM1yLv6dVDTPJnuPSsanGhrFo"
+
+const OFFER_ID = 4; // increment this to create new offers
 
 export default function EscrowPage() {
 
@@ -23,7 +25,8 @@ export default function EscrowPage() {
         // @ts-ignore
         escrowIDL, provider);
       const res = await program.methods.initialize(
-        new anchor.BN(1),
+        // increment this to create new offers
+        new anchor.BN(OFFER_ID),
         new anchor.BN(TOKEN_MINT_A_AMOUNT),
         new anchor.BN(TOKEN_MINT_B_AMOUNT),
         new anchor.web3.PublicKey(TAKER)
@@ -35,11 +38,40 @@ export default function EscrowPage() {
         })
         .rpc();
 
+      // transaction signature
       console.log(res);
     } catch (e) {
       console.error(e);
     }
   }, [])
+
+  // to get OTC offer accounts, simply derive PDA from seed in program
+  // or use Anchor
+  const offerDetails = useQuery({
+    queryKey: ["offerDetails", OFFER_ID],
+    queryFn: async () => {
+      try {
+        const provider = await anchorProvider();
+        const program = new anchor.Program<Escrow>(
+          // @ts-ignore
+          escrowIDL, provider);
+
+        const [offerPda, _] = PublicKey.findProgramAddressSync(
+          [
+            Buffer.from("otc_offer"),
+            provider.wallet.publicKey.toBuffer(), // maker seed 
+            new anchor.BN(OFFER_ID).toArrayLike(Buffer, "le", 8)
+          ],
+          program.programId
+        )
+
+        const offerDetails = await program.account.otcOffer.fetch(offerPda)
+        return offerDetails
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  })
 
   return <div>
     <div>
@@ -48,6 +80,15 @@ export default function EscrowPage() {
       <div>Token Mint B: {TOKEN_MINT_B}</div>
       <div>Token Mint B Amount: {TOKEN_MINT_B_AMOUNT}</div>
       <div>Taker: {TAKER}</div>
+      <div>Offer ID: {OFFER_ID}</div>
+    </div>
+    <div>
+      <div>Offer ID: {OFFER_ID}</div>
+      <div>Offer Details: 
+        <pre className="text-xs">
+          {offerDetails.data && JSON.stringify(offerDetails.data, null, 2)}
+        </pre>
+      </div>
     </div>
     <button className="my-4" onClick={depositEscrow}>
       Deposit Escrow
