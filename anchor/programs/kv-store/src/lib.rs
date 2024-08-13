91ac7e949f305a71d5bd2cf6e088fa65e8cdc65f
bao <baomai@berkeley.edu>
2024-08-13 16:09:19 -0700

    kv store using pda

---
+++ anchor/programs/kv-store/src/lib.rs
@@ -0,0 +1,38 @@
+use anchor_lang::prelude::*;
+
+declare_id!("CGKtFz7Vp3R1eruPU5VGvAFVQJn2WpR5Xo5hysYcKhzj");
+
+#[program]
+pub mod kv_store {
+    use super::*;
+
+    pub fn set_value(ctx: Context<SetValue>, _id: u64, _key: String, value: String) -> Result<()> {
+        ctx.accounts.kv_store.set_inner(KvStore { value });
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(id: u64, key: String)]
+pub struct SetValue<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = signer,
+        space = 8 + KvStore::INIT_SPACE,
+        seeds = [b"kv_store", signer.key().as_ref(), id.to_le_bytes().as_ref(), key.as_bytes()],
+        bump
+    )]
+    pub kv_store: Account<'info, KvStore>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct KvStore {
+    #[max_len(32)]
+    pub value: String,
+}
