b9706622f3eea698be8a22d9db72a6c748218afa
bao <baomai@berkeley.edu>
2024-08-13 20:55:38 -0700

    works

--- next-client/src/app/pubkey/page.tsx
+++ next-client/src/app/pubkey/page.tsx
@@ -3,41 +3,16 @@ import { anchorProvider } from "@/lib/util"
 import { Program } from "@coral-xyz/anchor";
 import { useCallback } from "react"
 import { pubkeyArgIDL, type PubkeyArg } from "anchor-local";
-import { PublicKey } from "@solana/web3.js";
+import { Keypair, PublicKey } from "@solana/web3.js";
 import { useQuery } from "@tanstack/react-query";
 import { SystemProgram } from "@solana/web3.js";
 
 
 const PUBKEY_TO_SAVE = "GriAght9Mi9m3BTKvPUM6426axdGayqCuqczczJrykdi";
 
+const pubkeyAccount = new Keypair();
+
 export default function PubkeyArg() {
-
-  const savePubkey = useCallback(async () => {
-    const provider = await anchorProvider();
-    const program = new Program<PubkeyArg>(
-      // @ts-expect-error
-      pubkeyArgIDL,
-      provider
-    );
-    const [pubkeyArgAddr] = PublicKey.findProgramAddressSync(
-      [
-        program.programId.toBuffer(),
-        new Uint8Array(pubkeyArgIDL.accounts.find((acc) => acc.name === "pubkeyArg")?.discriminator!)
-      ],
-      program.programId
-    );
-    const signature = await program.methods.initialize(new PublicKey(PUBKEY_TO_SAVE))
-      .accounts({
-        signer: provider.publicKey,
-        pubkeyArg: pubkeyArgAddr,
-        // @ts-expect-error
-        systemProgram: SystemProgram.programId,
-      })
-      .rpc();
-
-    await provider.connection.confirmTransaction(signature);
-    console.log("Signature", signature);
-  }, [])
 
   const savedPubkey = useQuery({
     queryKey: ["savedPubkey"],
@@ -48,17 +23,35 @@ export default function PubkeyArg() {
         pubkeyArgIDL,
         provider
       );
-      // seed: [programId, discriminator]
-      const [pubkeyArgAddr] = PublicKey.findProgramAddressSync(
-        [
-          program.programId.toBuffer(),
-          new Uint8Array(pubkeyArgIDL.accounts.find((acc) => acc.name === "pubkeyArg")?.discriminator!)
-        ],
-        program.programId
-      );
-      return program.account.pubkeyArg.fetch(pubkeyArgAddr);
+
+      return program.account.pubkeyArg.fetch(pubkeyAccount.publicKey);
     }
   })
+
+  const savePubkey = useCallback(async () => {
+    const provider = await anchorProvider();
+    const program = new Program<PubkeyArg>(
+      // @ts-expect-error
+      pubkeyArgIDL,
+      provider
+    );
+
+    const signature = await program.methods.initialize(new PublicKey(PUBKEY_TO_SAVE))
+      .accounts({
+        signer: provider.publicKey,
+        pubkeyArg: pubkeyAccount.publicKey,
+        // @ts-expect-error
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([pubkeyAccount])
+      .rpc();
+
+    await provider.connection.confirmTransaction(signature);
+    console.log("Signature", signature);
+    savedPubkey.refetch();
+  }, [savedPubkey])
+
+
 
 
   return <div>
