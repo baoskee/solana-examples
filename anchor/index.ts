f0c85a8eb45b30c7fd40b57e9d6f2be5f88f2733
bao <baomai@berkeley.edu>
2024-08-20 12:49:53 -0700

    spl demo client - ran into issue metadata program not local

--- anchor/index.ts
+++ anchor/index.ts
@@ -5,6 +5,7 @@ export { default as pubkeyArgIDL } from "./target/idl/pubkey_arg.json";
 export { default as reviewsVariableLenIDL } from "./target/idl/reviews_variable_len.json";
 export { default as smartWalletIDL } from "./target/idl/smart_wallet.json";
 export { default as cpiSignerIDL } from "./target/idl/cpi_signer.json";
+export { default as splDemoIDL } from "./target/idl/spl_demo.json";
 
 export * from "./target/types/escrow";
 export * from "./target/types/anchor_counter";
@@ -13,3 +14,4 @@ export * from "./target/types/pubkey_arg";
 export * from "./target/types/reviews_variable_len";
 export * from "./target/types/smart_wallet";
 export * from "./target/types/cpi_signer";
+export * from "./target/types/spl_demo";
