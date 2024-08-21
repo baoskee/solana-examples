e5c68f18c7aceb6ec2c6e63933c4eb26d858cc26
bao <baomai@berkeley.edu>
2024-08-21 14:55:29 -0700

    simple token initialize

--- anchor/programs/token-2022/src/lib.rs
+++ anchor/programs/token-2022/src/lib.rs
@@ -1,16 +1,80 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::rent::{
+    DEFAULT_EXEMPTION_THRESHOLD, DEFAULT_LAMPORTS_PER_BYTE_YEAR
+};
+use anchor_spl::token_interface::{Mint, Token2022, token_metadata_initialize, TokenMetadataInitialize};
+use anchor_lang::system_program::{transfer, Transfer};
+use spl_token_metadata_interface::state::TokenMetadata;
+use spl_type_length_value::variable_len_pack::VariableLenPack;
 
 declare_id!("QJqxGatfoyyHgeuxNgur5EWETdXGzYx2hBkdtrbT2gZ");
 
 #[program]
 pub mod token_2022 {
+
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        name: String,
+        symbol: String,
+        uri: String,
+    ) -> Result<()> {
+        let token_metadata = TokenMetadata {
+            name: name.clone(),
+            symbol: symbol.clone(),
+            uri: uri.clone(),
+            ..Default::default()
+        };
+
+        // Add 4 extra bytes for size of MetadataExtension (2 bytes for type, 2 bytes for length)
+        let data_len = 4 + token_metadata.get_packed_len()?;
+        // Calculate lamports required for the additional metadata
+        let lamports =
+            data_len as u64 * DEFAULT_LAMPORTS_PER_BYTE_YEAR * DEFAULT_EXEMPTION_THRESHOLD as u64;
+        // transfer additional lamports to the mint account
+        transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.signer.to_account_info(),
+                    to: ctx.accounts.mint.to_account_info(),
+                }
+            ),
+            lamports 
+        )?;
+        let ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            TokenMetadataInitialize {
+                token_program_id: ctx.accounts.token_program.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                metadata: ctx.accounts.mint.to_account_info(),
+                mint_authority: ctx.accounts.signer.to_account_info(),
+                update_authority: ctx.accounts.signer.to_account_info(),
+            }
+        );
+        token_metadata_initialize(ctx, name, symbol, uri)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        mint::decimals = 9,
+        mint::authority = signer,
+        extensions::metadata_pointer::authority = signer,
+        extensions::metadata_pointer::metadata_address = mint,
+    )]
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    // must be token-2022 for metadata extension
+    pub token_program: Program<'info, Token2022>,
+    pub system_program: Program<'info, System>,
+}
